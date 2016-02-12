1fba6b4e626ca6666a86ffcbdf7d90207f3cfd4b
Lars Finley Martinson <larsm_eyes@hotmail.com>
2016-02-12 13:35:57 -0700

    finito

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var $list = $('#peopleTable');
 var people = [personOne, personTwo, personThree];
 
 people.forEach(function(people){
-	$list.append('<tr> <td>' + people.firstName + '</td><td>' + people.lastName + '\
-		</td><td>' + people.phoneNumber + '</td><td><button class="btn btn-primary deleteTodo">done</button></td></tr>'
+	$list.append('<tr> 
+		<td>' + people.firstName + '</td>
+		<td>' + people.lastName + '</td>
+		<td>' + people.phoneNumber + '</td>
+		<td><button class="btn btn-primary deleteTodo">done</button></td></tr>'
 		)
 });
 
@@ -16,7 +19,7 @@ var deletePeople = function(event){
 	
 	alert("it worked")
 	$(event.target).closest('tr').remove();
- }
+}
 
 function createNewPersonFromForm(event){
 	
@@ -30,11 +33,14 @@ function createNewPersonFromForm(event){
 
 	if(firstName && lastName && phoneNumber){
 
-		$list.append('<tr> <td>' + firstName + '</td><td>' + lastName + '\
-			</td><td>' + phoneNumber + '</td><td><button class="btn btn-primary deleteTodo">done</button></td></tr>'
+		$list.append('<tr> 
+			<td>' + firstName + '</td>
+			<td>' + lastName + '</td>
+			<td>' + phoneNumber + '</td>
+			<td><button class="btn btn-primary deleteTodo">done</button></td></tr>'
 			)			
 
-		}
+	}
 }
 $('#submit-button').on('click', createNewPersonFromForm)
 $('.deletePeople').on('click', deletePeople)
